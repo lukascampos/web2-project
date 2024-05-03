9572f9aa04e8b9479af05c43cc3916dce6a01c7f
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2024-05-02 22:23:21 -0300

    criação de testes para instanciar entidade e criação de entidade abstrata compartilhada

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -4,3 +4,25 @@ export type UserProps = {
    password: string;
    createAt?: Date;
 };
+
+export class UserEntity {
+   constructor(public readonly props: UserProps) {
+      this.props.createAt = this.props.createAt ?? new Date();
+   }
+
+   get name(): string {
+      return this.props.name;
+   }
+
+   get email(): string {
+      return this.props.email;
+   }
+
+   get password(): string {
+      return this.props.password;
+   }
+
+   get createAt(): Date {
+      return this.props.createAt;
+   }
+}
